cc3c6f5eeef2484f531583dee3f91c7291be94d0
morishin <sntr92@gmail.com>
2025-02-25 09:58:37 +0900

    Remove unnecessary Spacer

--- XDeck/View/ContentView.swift
+++ XDeck/View/ContentView.swift
@@ -208,7 +208,6 @@ struct ContentView: View {
                                                 columnWidth: dynamicColumnWidth
                                             )
                                         }
-                                        Spacer()
                                     }
                                 }
                                 .alert(isPresented: $isShowingAlert) {
